1d4c11322c849376cbd01ea29b9a26bee29785c0
shah_kumar <96993218+Tshahjii@users.noreply.github.com>
2026-06-07 12:46:46 +0530

    NP migration create
    NP migration create

---
+++ database/migrations/2026_06_06_184345_create_master_n_p_team_mappings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('master_n_p_team_mappings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id');
+            $table->foreignId('state_id')->constrained('master_states')->cascadeOnDelete();
+            $table->foreignId('district_id')->constrained('master_districts')->cascadeOnDelete();
+            $table->foreignId('ward_id')->constrained('master_wards')->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained('master_cities')->cascadeOnDelete();
+            $table->foreignId('ps_id')->constrained('master_polling_stations')->cascadeOnDelete();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('master_n_p_team_mappings');
+    }
+};
---
+++ database/migrations/2026_06_06_184358_create_master_n_p_mappings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('master_n_p_mappings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained('master_n_p_team_mappings');
+            $table->enum('post_name', ['P0', 'P1', 'P2', 'P3']);
+            $table->foreignId('emp_id')->constrained('master_employees');
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('master_n_p_mappings');
+    }
+};
